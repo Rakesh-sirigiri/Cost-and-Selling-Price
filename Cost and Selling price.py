d32676c67050904bff123262e9b5b1bd82eb099a
Sirigiri Rakesh <94678816+Rakesh-sirigiri@users.noreply.github.com>
2024-07-15 12:07:18 +0530

    Add files via upload

---
+++ Cost and Selling price.py
@@ -0,0 +1,6 @@
+a = int(input("enter the marked price of a product"))
+CP = a - (a/5)
+SP = CP + (CP/5)
+print("cost price of a product is ", CP)
+print("selling price of a product is", SP)
+        
